d74783316400bea489692d74e61d1d6d2176b83d
Shez Abbas Soltani <shezsoltani@Mac.home>
2026-07-05 17:58:04 +0200

    Test Ordner und file angelegt #97

---
+++ backend/src/test/java/com/tourplanner/TourPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourplanner;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class TourPlannerApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
